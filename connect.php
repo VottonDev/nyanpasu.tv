f915bf571e637c25f5ddfb85c03ca99ab19d1ce9
Restyled.io <commits@restyled.io>
2022-07-11 19:33:06 +0000

    Restyled by php-cs-fixer

--- connect.php
+++ connect.php
@@ -1,23 +1,17 @@
 <?php
+
 require "configuration.php";
 
 try {
     $conn = new PDO("mysql:host=" . $servername . ";dbname=". $dbname, $username, $password);
     // set the PDO error mode to exception
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   if ($devmode == 1) {
-   ini_set('error_reporting', 0);
-   ini_set('display_errors', 0);
-}
-
-}
-
-
-catch(PDOException $e)
-    {
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if ($devmode == 1) {
+        ini_set('error_reporting', 0);
+        ini_set('display_errors', 0);
+    }
+} catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
     ini_set('error_reporting', 0);
     ini_set('display_errors', 0);
-    }
-
-?>
+}
